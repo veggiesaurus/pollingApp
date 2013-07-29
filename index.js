ed3a797d1c5100b020f4c607c3eddbca4fd2e285
Angus <accomrie@gmail.com>
2013-07-29 14:20:10 +0200

    removed flash and gzip from sockets

--- index.js
+++ index.js
@@ -41,16 +41,15 @@ io.set('log level', 5);                    // reduce logging
 
 io.enable('browser client minification');  // send minified client
 
-io.enable('browser client etag');          // apply etag caching logic based on version number
-io.enable('browser client gzip');          // gzip the file
+//io.enable('browser client etag');          // apply etag caching logic based on version number
+//io.enable('browser client gzip');          // gzip the file
 io.set('log level', 1);                    // reduce logging
 
 // enable all transports (optional if you want flashsocket support, please note that some hosting
 // providers do not allow you to create servers that listen on a port different than 80 or their
 // default port)
 io.set('transports', [
-    'websocket'
-  , 'flashsocket'
+    'websocket'  
   , 'htmlfile'
   , 'xhr-polling'
   , 'jsonp-polling'
